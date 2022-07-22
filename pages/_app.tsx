3100deba7ca5a48b020e74fcc2d320fd568012ec
UnbanksyTV <45092543+unbanksytv@users.noreply.github.com>
2022-07-22 22:09:40 +0200

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,29 @@
+import "../styles/globals.css";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import { ToastContainer } from "react-toastify";
+
+import 'react-toastify/dist/ReactToastify.min.css'
+
+export const activeChainId = ChainId.Rinkeby;
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThirdwebProvider desiredChainId={activeChainId}>
+      <Component {...pageProps} />
+      <ToastContainer 
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+    </ThirdwebProvider>
+  )
+}
+
+export default MyApp
