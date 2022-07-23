2146b69c11973e02af046ce71d23d573cf1c024a
UnbanksyTV <45092543+unbanksytv@users.noreply.github.com>
2022-07-23 10:01:53 +0200

    homepage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,31 @@
 import "../styles/globals.css";
+import Header from "../components/Header";
+import React from "react";
+import Head from "next/head";
+import type { AppProps } from "next/app";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import { ToastContainer } from "react-toastify";
-
 import 'react-toastify/dist/ReactToastify.min.css'
 
-export const activeChainId = ChainId.Rinkeby;
+// This is the chainId your dApp will work on.
+const activeChainId = ChainId.Rinkeby;
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider desiredChainId={activeChainId}>
+      <Head>
+        <title>thirdweb Marketplace with Next.JS</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content=""
+        />
+        <meta
+          name="keywords"
+          content=""
+        />
+      </Head>
+      <Header />
       <Component {...pageProps} />
       <ToastContainer 
         position="bottom-center"
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,86 @@
 import Head from "next/head";
+import type { NextPage } from "next";
+import Link from "next/link";
 import tw from "tailwind-styled-components";
+import styles from "../styles/Home.module.css";
+import {
+  MediaRenderer,
+  useActiveListings,
+  useMarketplace,
+} from "@thirdweb-dev/react";
+import { useRouter } from "next/router";
 
 // Components
 import Header from "../components/Header";
-import Main from "../components/Main";
 import Footer from "../components/Footer";
 
-export default function Home() {
-  return (
-    <Container>
-      <Head>
-        <title>The Photo Labs</title>
-        <meta name="description" content="Made with love by LiveTheLifeTV" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Header />
-      <Main />
-      <Footer />
-    </Container>
-  )
-}
+const Home: NextPage = () => {
+  const router = useRouter();
 
-const Container = tw.div`
- w-screen
- h-screen
- bg-gradient-color
- text-white
- px-7
- flex
- flex-col
- justify-between
-`
+  const marketplace = useMarketplace(
+    "0x3Fe6cfeb8530B3283c4d3E276BCC418FFdc0b748" 
+  );
+
+  const { data: listings, isLoading: loadingListings } =
+    useActiveListings(marketplace);
+
+
+
+    return (
+      <>
+        {/* Content */}
+        <div className={styles.container}>
+          {/* Top Section */}
+          <h1 className={styles.h1}>NFT Marketplace w/ thirdweb + Next.JS</h1>
+          <hr className={styles.divider} />
+  
+          <div style={{ marginTop: 32, marginBottom: 32 }}>
+            <Link href="/create">
+              <a className={styles.mainButton} style={{ textDecoration: "none" }}>
+                Create A Listing
+              </a>
+            </Link>
+          </div>
+  
+          <div className="main">
+            {
+              loadingListings ? (
+                <div>Loading listings ser...Stay Calm & Enjoy the Experiment</div>
+              ) : (
+                <div className={styles.listingGrid}>
+                  {listings?.map((listing) => (
+                    <div
+                      key={listing.id}
+                      className={styles.listingShortView}
+                      onClick={() => router.push(`/nfts/${listing.id}`)}
+                    >
+                      <MediaRenderer
+                        src={listing.asset.image}
+                        style={{
+                          borderRadius: 16,
+                          width: "100%",
+                          height: "100%",
+                        }}
+                      />
+                      <h2 className={styles.nameContainer}>
+                        <Link href={`/nfts/${listing.id}`}>
+                          <a className={styles.name}>{listing.asset.name}</a>
+                        </Link>
+                      </h2>
+  
+                      <p>
+                        <b>{listing.buyoutCurrencyValuePerToken.displayValue}</b>{" "}
+                        {listing.buyoutCurrencyValuePerToken.symbol}
+                      </p>
+                    </div>
+                  ))}
+                </div>
+              )
+            }
+          </div>
+        </div>
+      </>
+    );
+  };
+  
+export default Home;
