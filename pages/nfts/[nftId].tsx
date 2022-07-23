7d2ff7ea454750efce1f13c19ae23bea4c14cb3c
UnbanksyTV <45092543+unbanksytv@users.noreply.github.com>
2022-07-23 09:38:35 +0200

    dynamic page ListingID

--- pages/nfts/[nftId].tsx
+++
@@ -1,8 +0,0 @@
-import { useRouter } from 'next/router'
-
-export default function post(props) {
-const router = useRouter()
-console.log(router , 'routes')
-
-return ( <h2> nftId {router.query.id} </h2> )
-}
